2fae76e48a0004f20f9dd915adca9af318356b8a
David Leffley <github@leffo.com>
2015-09-15 23:24:14 +1000

    Initial commit

--- lifxbutton.ino
+++ lifxbutton.ino
@@ -1,5 +1,6 @@
 // This #include statement was automatically added by the Particle IDE.
 // Version 0.2
+// App to detect if a button is pressed on the Particle Internet Button and publish to MQTT.
 #include "MQTT/MQTT.h"
 #include "InternetButton/InternetButton.h"
 
@@ -24,10 +25,6 @@ MQTT client("mqtt.home.local", 1883, callback);
 
 // recieve message
 
-
-/* Here's a nice combination of features that I like to use.
-Note the use of the allButtons function. */
-
 InternetButton b = InternetButton();
 int red = 5;
 int green = 5;
@@ -35,15 +32,14 @@ int blue = 0;
 int lastButtonState[5] = {LOW,LOW,LOW,LOW,LOW};
 int connected = FALSE;
 int lastconnected = FALSE;
-std::string deviceID = "2b001e001647343339383037";
+String deviceID = System.deviceID();
+
 std::string topic = "";
 char* chrtopic;
 
 void setup() {
-    // Tell b to get everything ready to go
-    // Use b.begin(1); if you have the original SparkButton, which does not have a buzzer or a plastic enclosure
-    // to use, just add a '1' between the parentheses in the code above.
     b.begin();
+    // Connect mqtt to broker
     client.connect("mqtt.home.local");
 }
 
@@ -52,45 +48,44 @@ void loop(){
         client.loop();
         connected = TRUE;
         if (connected != lastconnected){
-            Spark.publish("reconnected", NULL, 60, PRIVATE);
+            Spark.publish("reconnected", NULL, 60, PRIVATE);    //publish the event for IFTTT to consume
             lastconnected = connected;
         }
 
         // Process individual buttons and LED response
-        if (b.buttonOn(1) != lastButtonState[1]) {
-            if (b.buttonOn(1)){
-                b.ledOn(12, 255, 0, 0); // Red
-                // Publish the event "button1" for other services like IFTTT to use
-                topic = "particle/" + deviceID + "/lights/Bedroom/button";
-                char* chrtopic = strdup(topic.c_str());     //need to convert string to char*
-                client.publish(chrtopic, "button for Bedroom clicked");
-                free(chrtopic);     //free memory for chrtopic
-                Spark.publish("Pressed", "Bedroom lights toggled.", 60, PRIVATE);
-                lastButtonState[1] = b.buttonOn(1);
-                delay(20);
+        if (b.buttonOn(1) != lastButtonState[1]) {                              //Button state has changed.  For edge detection.
+            if (b.buttonOn(1)){                                                 //The button is clicked.
+                b.ledOn(12, 255, 0, 0);                                         //Turn button bright red when pressed
+                topic = "particle/" + deviceID + "/buttons";                    //build topic
+                char* chrtopic = strdup(topic.c_str());                         //need to convert string to char
+                client.publish(chrtopic, "Button 1 Pressed");                   //publish click to MQTT.
+                free(chrtopic);                                                 //free memory. Not sure if this is needed.
+                Spark.publish("Pressed", "Button 1 Pressed", 60, PRIVATE);      //publish button press to particle cloud for IFTTT to consume.
+                lastButtonState[1] = b.buttonOn(1);                             //for edge detection
+                delay(20);                                                      //debounce
             }
-            else{
-                b.ledOn(12, red, green, blue);
-                lastButtonState[1] = b.buttonOn(1);
+            else{                                                               //The button is not clicked.
+                b.ledOn(12, red, green, blue);                                  //colour for the button not pressed
+                lastButtonState[1] = b.buttonOn(1);                             //for edge detection
             }
         }
         else{
-            if (lastButtonState[1]){
-                b.ledOn(12, 255, 0,0);
+            if (lastButtonState[1]){                                            //button is still clicked.  Don't take action, but keep the button red.
+                b.ledOn(12, 255, 0,0);                                          //Red
             }
             else{
-                b.ledOn(12, red, green, blue);
+                b.ledOn(12, red, green, blue);                                  //button is still not clicked.  Make/keep button default colour.
             }
         }
 
         if (b.buttonOn(2) != lastButtonState[2]) {
             if (b.buttonOn(2)){
                 b.ledOn(3, 255, 0, 0); // Red
-                // Publish the event "button1" for other services like IFTTT to use
-                topic = "particle/" + deviceID + "/lights/button";
+                topic = "particle/" + deviceID + "/buttons";
                 char* chrtopic = strdup(topic.c_str());
-                client.publish(chrtopic, "Set default colour");
-                Spark.publish("Pressed", "Lights set to default colours.", 60, PRIVATE);
+                client.publish(chrtopic, "Button 2 Pressed");
+                free(chrtopic);
+                Spark.publish("Pressed", "Button 2 Pressed", 60, PRIVATE);
                 lastButtonState[2] = b.buttonOn(2);
                 delay(20);
             }
@@ -111,9 +106,11 @@ void loop(){
         if (b.buttonOn(3) != lastButtonState[3]) {
             if (b.buttonOn(3)){
                 b.ledOn(6, 255, 0, 0); // Red
-                // Publish the event "button1" for other services like IFTTT to use
-                client.publish("particle/2b001e001647343339383037/lights/Study/button", "button for Study clicked");
-                Spark.publish("Pressed", "Study light toggled.", 60, PRIVATE);
+                topic = "particle/" + deviceID + "/buttons";
+                char* chrtopic = strdup(topic.c_str());
+                client.publish(chrtopic, "Button 3 Pressed");
+                free(chrtopic);
+                Spark.publish("Pressed", "Button 3 Pressed", 60, PRIVATE);
                 lastButtonState[3] = b.buttonOn(3);
                 delay(20);
             }
@@ -134,9 +131,11 @@ void loop(){
         if (b.buttonOn(4) != lastButtonState[4]) {
             if (b.buttonOn(4)){
                 b.ledOn(9, 255, 0, 0); // Red
-                // Publish the event "button1" for other services like IFTTT to use
-                client.publish("particle/2b001e001647343339383037/lights/Lounge/button", "button for Lounge clicked");
-                Spark.publish("Pressed", "Lounge lights toggled", 60, PRIVATE);
+                topic = "particle/" + deviceID + "/buttons";
+                char* chrtopic = strdup(topic.c_str());
+                client.publish(chrtopic, "Button 4 Pressed");
+                free(chrtopic);
+                Spark.publish("Pressed", "Button 4 Pressed", 60, PRIVATE);
                 lastButtonState[4] = b.buttonOn(4);
                 delay(20);
             }
--- mqttlifx.py
+++ mqttlifx.py
@@ -1,3 +1,7 @@
+#### need to put your deviceID here
+deviceID = 123456789
+delay = 500
+
 import lifx
 import paho.mqtt.client as mqtt
 from lifx.color import HSBK
@@ -13,46 +17,47 @@ def on_connect(client, userdata, rc):
 # The callback for when a PUBLISH message is received from the server.
 def on_message(client, userdata, msg):
     print "Topic: ", msg.topic+'\nMessage: '+str(msg.payload)
-    if str(msg.payload) == "button for Bedroom clicked":
-        print("message for Bedroom detected")
-        for l in lights.get_devices():
-            if l.label == "Davids":
-                print("Turning Davids off or on")
-                l.power_toggle()
-            elif l.label == "Robe":
-                print("Turning Robe off or on")
-                l.power_toggle()
-            elif l.label == "Shitter":
-                print("Turning Shitter off or on")
-                l.power_toggle()
-                client.publish(msg.topic, "Tried toggling Bedroom lights")
-    if str(msg.payload) == "Set default colour":
-        print("setting colour")
-        colour = HSBK(0,0,0.75,3200)
-        for l in lights.get_devices():
-            l.fade_color(colour)
-        client.publish(msg.topic, "Set lights to standard colour")
-    if str(msg.payload) == "button for Study clicked":
-        print("message for Study detected")
-        for l in lights.get_devices():
-            if l.label == "Study":
-                print("Turning Study off or on")
-                l.power_toggle()
-                client.publish(msg.topic, "Tried toggling Study lights")
+    if msg.topic == 'particle/' +deviceID + '/buttons':
+        if str(msg.payload) == 'Button 1 Pressed':
+            for l in lights.get_devices():
+                if l.label == "Bedroom1":
+                    l.power_toggle(delay)
+                elif l.label == "Robe":
+                    l.power_toggle(delay)
+                elif l.label == "Ensuite":
+                    l.power_toggle(delay)
+                    client.publish(msg.topic, "Tried toggling Bedroom lights")
 
-    if str(msg.payload) == "button for Lounge clicked":
-        print("message for Lounge detected")
-        for l in lights.get_devices():
-            label = l.label
-            if label.startswith('Lounge'):
-                print("Turning Lounge lights off or on")
-                l.power_toggle()
-        client.publish(msg.topic, "Tried toggling Lounge lights")
+        if str(msg.payload) == 'Button 2 Pressed':
+            print("setting colour")
+            colour = HSBK(0,0,0.75,3200)
+            for l in lights.get_devices():
+                l.fade_color(colour)
+            client.publish(msg.topic, "Set lights to standard colour")
+
+        if str(msg.payload) == 'Button 3 Pressed':
+            print("message for Study detected")
+            for l in lights.get_devices():
+                if l.label == "Study":
+                    print("Turning Study off or on")
+                    l.power_toggle(delay)
+                    client.publish(msg.topic, "Tried toggling Study lights")
+
+        if str(msg.payload) == 'Button 4 Pressed':
+            print("message for Lounge detected")
+            for l in lights.get_devices():
+                label = l.label
+                if label.startswith('Lounge'):
+                    print("Turning Lounge lights off or on")
+                    l.power_toggle(delay)
+            client.publish(msg.topic, "Tried toggling Lounge lights")
+
 
 lights = lifx.Client()
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
+
 
 client.connect("mqtt.home.local", 1883, 60)
 
