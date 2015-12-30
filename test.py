29050d849fe7fb8bb1e600e005a85303a59fc030
David Leffley <github@leffo.com>
2015-12-30 15:32:45 +1100

    Add first draft install instructions.  Remove sqlite setup

---
+++ test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+import os
+import code
+import lifx
+
+# Try to get readline if available
+try:
+    import readline
+except ImportError:
+    try:
+        import pyreadline as readline
+    except ImportError:
+        pass
+
+broadcast_addr = os.environ.get('LIFX_BROADCAST', '255.255.255.255')
+
+# Start the client
+lights = lifx.Client(broadcast=broadcast_addr)
+
+# Start interactive console
+shell = code.InteractiveConsole({'lights': lights})
+shell.interact(banner='Use the "lights" variable to use the SDK')
